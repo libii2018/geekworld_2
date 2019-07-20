13b683844502009d409b1cd68a5ef8ffdcecc085
Kratos <libiijoseph@gmail.com>
2019-07-20 19:24:29 +0200

    Menu hamburger

--- js/index.js
+++ js/index.js
@@ -6,3 +6,9 @@ $('.slider').slick({
 
 
 $('.carossels').slick();
+
+function OnClickMenu(){
+  document.getElementById("menu-bar").classList.toggle("total");
+  document.getElementById("menu").classList.toggle("change");
+  document.getElementById("nav_res").classList.toggle("change");
+}
